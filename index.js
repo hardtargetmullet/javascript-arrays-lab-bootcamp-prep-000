605e2f3e02dcd611249a280cb1ddb25a2924ee04
Abraham Nieto <elmeromero.an@gmail.com>
2017-12-19 12:22:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name) {
   return kittens.push(name)
 }
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+
